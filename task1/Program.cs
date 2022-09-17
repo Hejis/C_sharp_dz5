5c1f1ef41cd2d0d3c6f2c694d5c47f61217da961
simpaa_comp <simpaa@inbox.ru>
2022-09-17 18:16:06 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами.
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+int Prompt(string message)
+{
+  Console.Write(message);
+  string StringValue = Console.ReadLine() ?? "0";
+  bool isNumber = int.TryParse(StringValue, out int value);
+  if (isNumber)
+  {
+    return value;
+  }
+  throw new Exception("Данное значение невозможно преобразовать в число");
+
+}
+
+int len = Prompt("Введите длину одномерного массива: ");
+int[] arr = new int[len];
+            int k = 0;
+            Random rnd = new Random();
+ 
+            Console.WriteLine("Исходный массив");
+            for (int i = 0; i < len; i++)
+            {
+                arr[i] = rnd.Next(100, 1000);
+                Console.Write(" " +  arr[i]);
+            }
+            Console.WriteLine();
+            
+            foreach(int element in arr)
+            {
+                if (element % 2 == 0)
+                {
+                    k++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Количество четных элементов " + k );
+
+
+            
